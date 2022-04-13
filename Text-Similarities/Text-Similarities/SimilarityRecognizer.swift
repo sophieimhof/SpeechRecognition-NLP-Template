89df678ab036e196f434fa4a117c38cbda9e37b6
simhof3 <simhof3@gatech.edu>
2022-04-13 12:22:08 -0400

    added descriptive comments

--- Text-Similarities/Text-Similarities/SimilarityRecognizer.swift
+++ Text-Similarities/Text-Similarities/SimilarityRecognizer.swift
@@ -10,6 +10,7 @@ import Foundation
 
 class SimilarityRecognizer {
     
+    //finds the "numWords" most similar words to "word" and the relative distance to the "word"
     func findSimilarWords(word: String, numWords: Int) -> [(String, NLDistance)] {
         if let embedding = NLEmbedding.wordEmbedding(for: .english) {
             let similarWords = embedding.neighbors(for: word, maximumCount: numWords)
@@ -18,6 +19,7 @@ class SimilarityRecognizer {
         return Array<(String, Double)>()
     }
     
+    //finds the distance(similarity) between two strings
     func compareSentenceSimilarity(sentence1: String, sentence2: String) -> NLDistance {
         if let embedding = NLEmbedding.sentenceEmbedding(for: .english) {
             let distance = embedding.distance(between: sentence1, and: sentence2)
--- Text-Similarities/Text-Similarities/ViewController.swift
+++ Text-Similarities/Text-Similarities/ViewController.swift
@@ -10,44 +10,46 @@ import NaturalLanguage
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var word1: UITextField!
-    @IBOutlet weak var similarWordsButton: UIButton!
-    @IBOutlet weak var similarWordsText: UILabel!
+    @IBOutlet weak var word1: UITextField! //text field for user input of a word
+    @IBOutlet weak var similarWordsButton: UIButton! //button that finds similar words to user input word
+    @IBOutlet weak var similarWordsText: UILabel! //text for similar words output
     
     
-    @IBOutlet weak var sentence1: UITextField!
-    @IBOutlet weak var sentence2: UITextField!
-    @IBOutlet weak var similarSentButton: UIButton!
-    @IBOutlet weak var similarSentText: UILabel!
+    @IBOutlet weak var sentence1: UITextField! //text field for user input sentence 1
+    @IBOutlet weak var sentence2: UITextField! //text field for user input sentence 2
+    @IBOutlet weak var similarSentButton: UIButton! //button that finds distance between two sentences
+    @IBOutlet weak var similarSentText: UILabel! //text to display distance
     
-    var simiarityRecognizer: SimilarityRecognizer = SimilarityRecognizer()
+    var simiarityRecognizer: SimilarityRecognizer = SimilarityRecognizer() //create a similarity recognizer object
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    //finds similar words when button is pressed
     @IBAction func wordButtonTouch(_ sender: Any) {
         var similarWords: [(String, NLDistance)]
         var returnText: String = ""
-        similarWords = simiarityRecognizer.findSimilarWords(word: word1.text ?? "word", numWords: 5)
+        similarWords = simiarityRecognizer.findSimilarWords(word: word1.text ?? "word", numWords: 5) //default text is word
         if (similarWords.isEmpty) {
-            returnText = "Not a valid word."
+            returnText = "Not a valid word." //returns this response if findSimilarWords fails
         } else {
             for words in similarWords {
                 returnText = returnText + String(format: words.0 + ", %.4f\n", words.1)
-            }
+            } //formats similar words with distances
         }
-        similarWordsText.text = returnText
+        similarWordsText.text = returnText //sets UI text to return text
     }
     
+    //finds similarity between sentences when button is pressed
     @IBAction func sentButtonClick(_ sender: Any) {
         if (sentence1.text == nil || sentence2.text == nil) {
-            similarSentText.text = "Not a valid input."
+            similarSentText.text = "Not a valid input." //checks for valid input
         } else {
             var distance: NLDistance
             distance = simiarityRecognizer.compareSentenceSimilarity(sentence1: sentence1.text ?? "This is not a valid input." , sentence2: sentence2.text ?? "This is not a valid input")
-        similarSentText.text = String(format: "Distance: %.4f", distance)
+        similarSentText.text = String(format: "Distance: %.4f", distance) //formats response distance from compareSentenceSimilarity function
         }
     }
 
