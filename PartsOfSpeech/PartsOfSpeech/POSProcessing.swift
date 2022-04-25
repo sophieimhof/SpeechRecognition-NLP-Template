7a34962ebb5bf744aab6278de48eb71d59d850d7
sanjanasrinivasan29 <98435328+sanjanasrinivasan29@users.noreply.github.com>
2022-04-25 00:57:38 -0400

    Update POSProcessing.swift

--- PartsOfSpeech/PartsOfSpeech/POSProcessing.swift
+++ PartsOfSpeech/PartsOfSpeech/POSProcessing.swift
@@ -2,7 +2,7 @@
 //  TextProcessing.swift
 //  SpeechToText
 //
-//  Created by Sharay Gao on 4/11/22.
+//  Created by Sanjana Srinivasan on 4/11/22.
 //
 
 import NaturalLanguage
@@ -14,18 +14,20 @@ import Speech
 class POSProcessing {
     
     func findPartsOfSpeech(text: String) -> Dictionary<String, Array<String>>{
-        var returnValue = Dictionary<String, Array<String>>()
+        var returnValue = Dictionary<String, Array<String>>() //dictionary of parts of speech from the inputted text
         
         //let text = "What day is today"
         let tagger = NLTagger(tagSchemes: [.lexicalClass])
         tagger.string = text
-        let options: NLTagger.Options = [.omitPunctuation, .omitWhitespace]
+        let options: NLTagger.Options = [.omitPunctuation, .omitWhitespace] //leave out punctuation and white spaces
+        //enumerate through range of text using tags
         tagger.enumerateTags(in: text.startIndex..<text.endIndex, unit: .word, scheme: .lexicalClass, options: options) { tag, tokenRange in
             if let tag = tag {
-                //print("\(text[tokenRange]): \(tag.rawValue)")
+                //print("\(text[tokenRange]): \(tag.rawValue)") to print each word with part of speech on new line
                 let word: String = String(text[tokenRange])
                 let wordType = tag.rawValue
                 
+                //add each word to the dictionary with its associated part of speech
                 if returnValue[wordType] == nil {
                     var words: [String] = []
                     words.append(word)
@@ -41,7 +43,7 @@ class POSProcessing {
             return true
         }
             
-        return returnValue
+        return returnValue //parts of speech dictionary
     }
     
 }
